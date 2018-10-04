4875ddc2c88ce9b3df685466371eaf79744c3765
Dan Florio <dflo@danflorio.com>
2018-10-04 11:11:32 -0400

    remove unnecessary packages and add animation

--- src/index.js
+++ src/index.js
@@ -3,28 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import { Provider } from 'react-redux';
-import rootReducer from './reducers';
 
-let middleware = [thunk];
-
-if (process.env.NODE_ENV !== 'production') {
-  middleware = [ ...middleware, createLogger() ]
-}
-
-const store = createStore(
-  rootReducer,
-  applyMiddleware(...middleware)
-);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+  <App />, document.getElementById('root')
 );
 
 serviceWorker.unregister();
--- src/parts/header/Header.js
+++ src/parts/header/Header.js
@@ -2,6 +2,7 @@
 import React from 'react';
 
 // Assets
+import 'animate.css';
 import './Header.css';
 import sun from '../../assets/img/sun.svg';
 import cloud1 from '../../assets/img/cloud1.svg';
@@ -10,7 +11,9 @@ import cloud2 from '../../assets/img/cloud2.svg';
 
 const Header = () => (
   <div className="header">
-    <img className="header__sun" src={sun} />
+    <span className="header__sun-container">
+      <img className="header__sun" src={sun} />
+    </span>
     <img className="header__cloud header__cloud--1" src={cloud1} />
     <img className="header__cloud header__cloud--2" src={cloud2} />
     <h1 className="header__title">
