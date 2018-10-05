138ccd2137aeadc2169b2bc93c9a9c1d56f2ed20
Dan Florio <dflo@danflorio.com>
2018-10-04 21:37:58 -0400

    style results and add temp

--- src/components/weather-data/WeatherData.js
+++ src/components/weather-data/WeatherData.js
@@ -6,57 +6,92 @@ import moment from 'moment';
 // Assets
 import './WeatherData.css';
 
+
+const Thermometer = (props) => {
+
+  const max = props.units === "imperial" ? 100 : 38;
+  let width = (props.temp / max) * 100;
+  if (width < 0) {
+    width = 0;
+  } else if (width > 100) {
+    width = 100;
+  }
+
+  const style = {
+    width: width
+  }
+
+  return (
+    <div className="thermometer">
+      <span className="thermometer__temp">{Math.round(props.temp)}&deg;</span>
+      <div className="thermometer__fill" style={style}></div>
+    </div>
+  );
+}
+
+
 const Entry = (props) => {
 
   const day = moment(props.time.split(' ')[0], 'YYYY-MM-DD');
   const parsedDay = day.format('ddd, MMM Do');
   const time = moment(props.time.split(' ')[1], 'HH:mm:ss');
   const parsedTime = time.format('h:mm a');
-  console.log(parsedTime);
   
   return (
-    <li className="entry">
-      <div className="entry__date">
-        <p className="entry__date-day">
-          {parsedDay}
-        </p>
-        <p className="entry__date-time">
-          {parsedTime}
-        </p>
-      </div>
-      <img className="entry__icon" src={props.icon} alt="weather icon" />
-      { props.weather }
-    </li>
+    <tr className="entry">
+      <td>
+        <div className="entry__date">
+          <p className="entry__date-day">
+            {parsedDay}
+          </p>
+          <p className="entry__date-time">
+            {parsedTime}
+          </p>
+        </div>
+      </td>
+      <td>
+        <img className="entry__icon" src={props.icon} alt="weather icon" />
+      </td>
+      <td>
+        <p className="entry__description">{ props.weather }</p>
+      </td>
+      <td>
+        <Thermometer units="imperial" temp={props.temp} />
+      </td>
+    </tr>
   );
 };
 
 const WeatherData = (props) => (
   <div className="weather-data">
-    <ul className="weather-data__list">
+    <table className="weather-data__list">
+      <tbody>
       {
         props.data.map(entry => (
           <Entry
             key={entry.dt}
             icon={"http://openweathermap.org/img/w/" + entry.weather[0].icon + ".png"}
             time={entry.dt_txt}
-            weather={entry.weather[0].main}
+            weather={entry.weather[0].description}
             temp={entry.main.temp}
-            tempMin={entry.main.temp_min}
-            tempMax={entry.main.temp_max}
           />
         ))
       }
-    </ul>
+      </tbody>
+    </table>
   </div>
 );
+
+Thermometer.propTypes = {
+  units: PropTypes.string.isRequired,
+  temp: PropTypes.number.isRequired
+}
 
 Entry.propTypes = {
   icon: PropTypes.string.isRequired,
   time: PropTypes.string.isRequired,
   weather: PropTypes.string.isRequired,
-  temp: PropTypes.number.isRequired,
-  tempMin: PropTypes.number.isRequired,
-  tempMax: PropTypes.number.isRequired
+  temp: PropTypes.number.isRequired
 }
 
 WeatherData.propTypes = {
