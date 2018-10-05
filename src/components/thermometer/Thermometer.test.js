c015caa33dfeb0adcc700be1e23e2372fc172d2b
Dan Florio <dflo@danflorio.com>
2018-10-05 13:52:04 -0400

    add testing packages and thermometer tests

---
+++ src/components/thermometer/Thermometer.test.js
@@ -0,0 +1,25 @@
+import Thermometer from './Thermometer';
+
+it("renders correctly", () => {
+  const wrapper = shallow(
+    <Thermometer
+      units="imperial"
+      temp={70}
+    />
+  );
+
+  expect(wrapper).toMatchSnapshot();
+});
+
+it("rounds the temperature", () => {
+  const wrapper = mount(
+    <Thermometer
+      units="imperial"
+      temp={70.6}
+    />
+  );
+
+  const text = wrapper.find(".thermometer__temp").text();
+
+  expect(text).toEqual("71°");
+});
